90e40b0884143f27f498ce71facdfde30de800be
ILIYA LESANI <iliyalesani@gmail.com>
2026-02-13 23:15:35 +0330

    feat: initialize main window
    - sets profile and opengl version
    - creates window and defines color buffer
    - sets polling to prevent unresponsiveness

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,31 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 int main() {
+    const int windowWidth = 1280;
+    const int windowHeight = 720;
 	glfwInit();
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(windowWidth, windowHeight, "GLVK", NULL, NULL);
+	if( window == NULL) {
+	    return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	gladLoadGL();
+	glViewport(0, 0, windowWidth, windowHeight);
+	glClearColor(0.05f, 0.15f, 0.20f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glfwSwapBuffers(window);
+
+	while(!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
 	glfwTerminate();
 	return 0;
 }
